1b6c790cee36d4f256a4a314893349d4a9852122
Francisco Jordan Garcia <franjoga24@gmail.com>
2023-07-11 13:51:33 +0200

    Java hasta la mitad

--- Ejercicios/Java/Ejercicio 11/Ejercicio11.java
+++ Ejercicios/Java/Ejercicio 11/Ejercicio11.java
@@ -1,8 +1,18 @@
 import java.lang.Math;
 
 public class Ejercicio11 {
+    public static void blancoONegro() {
+        int numero = (int) (Math.round(Math.random()));
+        if (numero != 0) {
+            System.out.println("Negro");
+        } else {
+            System.out.println("Blanco");
+        }
+    }
     public static void main (String[] args){
         int numero = Integer.parseInt(args[0]);
         System.out.println((numero % 2 == 0)?"par":"impar");
+
+        blancoONegro();
     }
 }
